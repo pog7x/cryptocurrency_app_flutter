3fb3df5f89244ecfe0f1b57c75400f5383098368
pog7x <poluningm@gmail.com>
2023-07-03 00:44:35 +0300

    rm gesture detector, default suggestions in search, fix user settings overflow

--- lib/features/crypto_coin_search/view/crypto_coin_search_screen.dart
+++ lib/features/crypto_coin_search/view/crypto_coin_search_screen.dart
@@ -23,109 +23,107 @@ class _CryptoCoinSearchScreenState extends State<CryptoCoinSearchScreen> {
   final userSettingsRepo = GetIt.I<AbstractUserSettingsRepository>();
   final cryptoCoinRepo = GetIt.I<AbstractCryptoCoinRepository>();
 
-  List<String> _getAllCoins(String pattern) {
-    return allCryptoCoins
-        .where(
-          (i) => i.toLowerCase().startsWith(
-                pattern.toLowerCase(),
-              ),
-        )
-        .toList();
-  }
-
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final focusNode = FocusNode(canRequestFocus: true);
-    return GestureDetector(
-      onTap: () => focusNode.unfocus(),
-      onVerticalDragStart: (e) => focusNode.unfocus(),
-      onHorizontalDragStart: (e) => focusNode.unfocus(),
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('Search'),
-          leading: const AutoLeadingButton(),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 10),
-              const Text('Put your search request:'),
-              Container(
-                padding: const EdgeInsets.all(16),
-                child: TypeAheadField(
-                  hideKeyboardOnDrag: true,
-                  keepSuggestionsOnSuggestionSelected: true,
-                  hideSuggestionsOnKeyboardHide: false,
-                  loadingBuilder: (context) => Center(
-                    child: Container(
-                      alignment: Alignment.center,
-                      width: double.infinity,
-                      height: double.infinity,
-                      decoration: const BoxDecoration(
-                        color: Color.fromARGB(255, 31, 31, 31),
-                      ),
-                      child: const CircularProgressIndicator(),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Search'),
+        leading: const AutoLeadingButton(),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            const Text('Put your search request:'),
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: TypeAheadField(
+                hideKeyboardOnDrag: true,
+                hideSuggestionsOnKeyboardHide: false,
+                loadingBuilder: (BuildContext context) => Center(
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: double.infinity,
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                      color: theme.scaffoldBackgroundColor,
                     ),
+                    child: const CircularProgressIndicator(),
                   ),
-                  noItemsFoundBuilder: (context) => Center(
-                    child: Container(
-                      alignment: Alignment.center,
-                      width: double.infinity,
-                      height: double.infinity,
-                      decoration: const BoxDecoration(
-                        color: Color.fromARGB(255, 31, 31, 31),
-                      ),
-                      child: Text(
-                        'No items found',
-                        style: theme.textTheme.bodyMedium,
-                      ),
-                    ),
-                  ),
-                  itemSeparatorBuilder: (context, i) => const Divider(
-                    height: .5,
-                  ),
-                  textFieldConfiguration: TextFieldConfiguration(
-                    autofocus: true,
-                    focusNode: focusNode,
-                    style: theme.textTheme.labelMedium,
-                    decoration: const InputDecoration(
-                      hintText: 'e.g., BTC',
-                    ),
-                  ),
-                  suggestionsCallback: (pattern) async {
-                    final searchRequest = _getAllCoins(pattern);
-                    if (pattern != '' && searchRequest.isNotEmpty) {
-                      return cryptoCoinRepo.getCryptoCoinList(
-                        userSettingsRepo.getUserSettings().favCurrency,
-                        searchRequest,
-                      );
-                    }
-                    return <CryptoCoin>[];
-                  },
-                  itemBuilder: (context, CryptoCoin item) {
-                    return CryptoCoinListTile(
-                      cryptoCoin: item,
-                      userSettings: userSettingsRepo.getUserSettings(),
-                      userSettingsRepo: userSettingsRepo,
-                    );
-                  },
-                  onSuggestionSelected: (item) {
-                    AutoRouter.of(context)
-                        .push(
-                          CryptoCoinRoute(
-                            cryptoCoin: item,
-                          ),
-                        )
-                        .then((value) => focusNode.requestFocus());
-                  },
                 ),
+                noItemsFoundBuilder: (BuildContext context) => Center(
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: double.infinity,
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                      color: theme.scaffoldBackgroundColor,
+                    ),
+                    child: Text(
+                      'No items found',
+                      style: theme.textTheme.bodyMedium,
+                    ),
+                    // child: const CircularProgressIndicator(),
+                  ),
+                ),
+                itemSeparatorBuilder: (context, i) => const Divider(
+                  height: .5,
+                ),
+                textFieldConfiguration: TextFieldConfiguration(
+                  autofocus: true,
+                  focusNode: focusNode,
+                  onTapOutside: (PointerDownEvent event) => focusNode.unfocus(),
+                  style: theme.textTheme.labelMedium,
+                  decoration: const InputDecoration(
+                    hintText: 'e.g., BTC',
+                  ),
+                ),
+                suggestionsCallback: (String pattern) async {
+                  final searchRequest = _getCoinsNames(pattern);
+                  if (searchRequest.isNotEmpty) {
+                    return cryptoCoinRepo.getCryptoCoinList(
+                      userSettingsRepo.getUserSettings().favCurrency,
+                      searchRequest,
+                    );
+                  }
+                  return <CryptoCoin>[];
+                },
+                itemBuilder: (BuildContext context, CryptoCoin coin) {
+                  return CryptoCoinListTile(
+                    cryptoCoin: coin,
+                    userSettings: userSettingsRepo.getUserSettings(),
+                    userSettingsRepo: userSettingsRepo,
+                  );
+                },
+                onSuggestionSelected: (CryptoCoin coin) {
+                  AutoRouter.of(context).push(
+                    CryptoCoinRoute(
+                      cryptoCoin: coin,
+                    ),
+                  );
+                },
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  List<String> _getCoinsNames(String pattern) {
+    if (pattern.isNotEmpty) {
+      return allCryptoCoins
+          .where(
+            (i) => i.toLowerCase().startsWith(
+                  pattern.toLowerCase(),
+                ),
+          )
+          .toList();
+    } else {
+      return allCryptoCoins.sublist(0, 15);
+    }
   }
 }
--- lib/features/user_settings/view/user_settings_screen.dart
+++ lib/features/user_settings/view/user_settings_screen.dart
@@ -46,81 +46,83 @@ class _UserSettingsScreenState extends State<UserSettingsScreen> {
           if (state is UserSettingsLoaded) {
             final repo = GetIt.I<AbstractUserSettingsRepository>();
             final userSettings = state.userSettings;
-            return Column(
-              children: [
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'Select currency',
-                  style: theme.textTheme.bodyMedium,
-                ),
-                BaseCard(
-                  child: ListView.separated(
-                    shrinkWrap: true,
-                    itemCount: currencyItems.length,
-                    separatorBuilder: (context, i) => const Divider(),
-                    itemBuilder: (context, i) {
-                      final currName = currencyItems[i];
-                      return ListTile(
-                        title: Text(
-                          currName,
-                          style: theme.textTheme.bodyMedium,
-                        ),
-                        onTap: () {
-                          repo.putUserSettings(
-                            UserSettings(
-                              favCurrency: currName,
-                              appThemeMode: userSettings.appThemeMode,
-                              likedCryptocoins: userSettings.likedCryptocoins,
-                            ),
-                          );
-                          _userSettingsBloc.add(LoadUserSettings());
-                        },
-                        trailing: (userSettings.favCurrency == currName)
-                            ? const Icon(Icons.check)
-                            : const Icon(null),
-                      );
-                    },
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 10,
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'Select theme mode',
-                  style: theme.textTheme.bodyMedium,
-                ),
-                BaseCard(
-                  child: ListView.separated(
-                    shrinkWrap: true,
-                    itemCount: appThemeModeItems.length,
-                    separatorBuilder: (context, i) => const Divider(),
-                    itemBuilder: (context, i) {
-                      final themeMode = appThemeModeItems[i];
-                      return ListTile(
-                        title: Text(
-                          themeMode,
-                          style: theme.textTheme.bodyMedium,
-                        ),
-                        onTap: () {
-                          repo.putUserSettings(
-                            UserSettings(
-                              appThemeMode: themeMode,
-                              favCurrency: userSettings.favCurrency,
-                              likedCryptocoins: userSettings.likedCryptocoins,
-                            ),
-                          );
-                          _userSettingsBloc.add(LoadUserSettings());
-                        },
-                        trailing: (userSettings.appThemeMode == themeMode)
-                            ? const Icon(Icons.check)
-                            : const Icon(null),
-                      );
-                    },
+                  Text(
+                    'Select currency',
+                    style: theme.textTheme.bodyMedium,
                   ),
-                ),
-              ],
+                  BaseCard(
+                    child: ListView.separated(
+                      shrinkWrap: true,
+                      itemCount: currencyItems.length,
+                      separatorBuilder: (context, i) => const Divider(),
+                      itemBuilder: (context, i) {
+                        final currName = currencyItems[i];
+                        return ListTile(
+                          title: Text(
+                            currName,
+                            style: theme.textTheme.bodyMedium,
+                          ),
+                          onTap: () {
+                            repo.putUserSettings(
+                              UserSettings(
+                                favCurrency: currName,
+                                appThemeMode: userSettings.appThemeMode,
+                                likedCryptocoins: userSettings.likedCryptocoins,
+                              ),
+                            );
+                            _userSettingsBloc.add(LoadUserSettings());
+                          },
+                          trailing: (userSettings.favCurrency == currName)
+                              ? const Icon(Icons.check)
+                              : const Icon(null),
+                        );
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Select theme mode',
+                    style: theme.textTheme.bodyMedium,
+                  ),
+                  BaseCard(
+                    child: ListView.separated(
+                      shrinkWrap: true,
+                      itemCount: appThemeModeItems.length,
+                      separatorBuilder: (context, i) => const Divider(),
+                      itemBuilder: (context, i) {
+                        final themeMode = appThemeModeItems[i];
+                        return ListTile(
+                          title: Text(
+                            themeMode,
+                            style: theme.textTheme.bodyMedium,
+                          ),
+                          onTap: () {
+                            repo.putUserSettings(
+                              UserSettings(
+                                appThemeMode: themeMode,
+                                favCurrency: userSettings.favCurrency,
+                                likedCryptocoins: userSettings.likedCryptocoins,
+                              ),
+                            );
+                            _userSettingsBloc.add(LoadUserSettings());
+                          },
+                          trailing: (userSettings.appThemeMode == themeMode)
+                              ? const Icon(Icons.check)
+                              : const Icon(null),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             );
           }
           return const Center(
